983abdbb6b6ab2d1913fee5500835c8b0339df9f
nataliia-v <nataly.verbenskaya@gmail.com>
2024-03-19 13:26:47 +0200

    fix: routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,26 +6,26 @@ const Navbar = () => {
     <header className="header">
       Navbar
       <NavLink
-        to="/"
+        to="/portfolio"
         className="w-20 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
       >
         <p className="green-gradient_text">Home</p>
       </NavLink>
       <nav className="flex text-lg gap-7 font-medium">
         <NavLink
-          to="/about"
+          to="/portfolio/about"
           className={({ isActive }) => (isActive ? 'text-lime-500' : 'text-black')}
         >
           About
         </NavLink>
         <NavLink
-          to="/projects"
+          to="/portfolio/projects"
           className={({ isActive }) => (isActive ? 'text-lime-500' : 'text-black')}
         >
           Projects
         </NavLink>
         <NavLink
-          to="/contact"
+          to="/portfolio/contact"
           className={({ isActive }) => (isActive ? 'text-lime-500' : 'text-black')}
         >
           Contact
