159e64efab0514cb1e0b77d68501ad75401c4914
nataliia-v <nataly.verbenskaya@gmail.com>
2024-03-25 14:19:08 +0200

    fix: removed console log

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,8 +13,6 @@ const Contact = () => {
     e.preventDefault();
     setIsLoading( true );
 
-    console.log('pk', import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY)
-
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
