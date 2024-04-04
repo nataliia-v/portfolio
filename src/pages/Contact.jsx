5bfc87bdd299ea841fcb76173b56f17439b24af4
nataliia-v <nataly.verbenskaya@gmail.com>
2024-04-04 17:21:21 +0300

    fix: mobile Contacts container

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -78,9 +78,7 @@ const Contact = () => {
     <section className="relative flex lg:flex-row flex-col max-container">
       {alert.show && <Alert {...alert} />}
       <div className="flex-1 min-w-[50%] flex flex-col">
-        <h1>Get in Touch</h1>
-
-        <form className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit}>
+        <form className="w-full flex flex-col gap-5 mt-14" onSubmit={handleSubmit}>
           <label className="text-black-500 font-semibold">
             Name
             <input
